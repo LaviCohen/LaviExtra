8db79481da35d6974ae20f101112ad98608ddf44
lavi cohen <lavicohen78@gamil.com>
2022-07-24 23:04:19 +0300

    Fix Messages Icons

--- LaviExtra/LaviExtra/le/gui/dialogs/LDialogs.java
+++ LaviExtra/LaviExtra/le/gui/dialogs/LDialogs.java
@@ -26,69 +26,68 @@ import le.gui.components.LTextField;
 import le.utils.VarHolder;
 
 /**
- * Replacement for @see java.swing.JOptionPane, to support another LaviExtra features,
- * such as translation and themes. 
- * */
+ * Replacement for @see java.swing.JOptionPane, to support another LaviExtra
+ * features, such as translation and themes.
+ */
 public class LDialogs {
-	
+
 	public static final int WARNING_MESSAGE = 1;
 	public static final int ERROR_MESSAGE = 2;
 	public static final int INFO_MESSAGE = 3;
 	public static final int QUESTION_MESSAGE = 4;
-	
+
 	public static final int YES_OPTION = 0;
 	public static final int NO_OPTION = 1;
 	public static final int CANCEL_OPTION = 2;
-	
+
 	public static ColorTheme theme = ColorTheme.DEFAULT_COLOR_THEME;
-	
+
 	private static Icon warningIcon;
 	private static Icon errorIcon;
 	private static Icon infoIcon;
 	private static Icon questionIcon;
-	
-	
+
 	static {
 		try {
-			warningIcon = getIcon(ImageIO.read(
-					LDialogs.class.getResourceAsStream("/le/resources/Warning.png")), 50, 50);
-			errorIcon = getIcon(ImageIO.read(
-					LDialogs.class.getResourceAsStream("/le/resources/Error.png")), 50, 50);
-			infoIcon = getIcon(ImageIO.read(
-					LDialogs.class.getResourceAsStream("/le/resources/Info.png")), 50, 50);
-			questionIcon = getIcon(ImageIO.read(
-					LDialogs.class.getResourceAsStream("/le/resources/Question.png")), 50, 50);
+			warningIcon = getIcon(ImageIO.read(LDialogs.class.getResourceAsStream("/le/resources/Warning.png")), 50,
+					50);
+			errorIcon = getIcon(ImageIO.read(LDialogs.class.getResourceAsStream("/le/resources/Error.png")), 50, 50);
+			infoIcon = getIcon(ImageIO.read(LDialogs.class.getResourceAsStream("/le/resources/Info.png")), 50, 50);
+			questionIcon = getIcon(ImageIO.read(LDialogs.class.getResourceAsStream("/le/resources/Question.png")), 50,
+					50);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static ImageIcon getIcon(Image image, int w, int h) {
 		return new ImageIcon(getScaledImage(image, w, h));
 	}
-	
-	public static BufferedImage getScaledImage(Image srcImg, int width, int height){
-	    BufferedImage resizedImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-	    Graphics2D g2 = resizedImg.createGraphics();
-	    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	    g2.drawImage(srcImg, 0, 0, width, height, null);
-	    g2.dispose();
-	    return resizedImg;
+
+	public static BufferedImage getScaledImage(Image srcImg, int width, int height) {
+		BufferedImage resizedImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g2 = resizedImg.createGraphics();
+		g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2.drawImage(srcImg, 0, 0, width, height, null);
+		g2.dispose();
+		return resizedImg;
 	}
-	
+
 	private static JDialog createDialog(Component owner, String title) {
 		JDialog d = new JDialog(getWindowParent(owner), title, ModalityType.APPLICATION_MODAL);
 		d.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		d.getContentPane().setBackground(theme.getBackgroundColor());
 		return d;
 	}
+
 	private static Window getWindowParent(Component owner) {
 		if (owner == null || owner instanceof Window) {
 			return (Window) owner;
 		}
 		return getWindowParent(owner.getParent());
 	}
+
 	private static Icon getIcon(int type) {
 		switch (type) {
 		case ERROR_MESSAGE:
@@ -103,22 +102,27 @@ public class LDialogs {
 			throw new IllegalArgumentException("Unexpected value: " + type);
 		}
 	}
+
 	public static void showMessageDialog(Component owner, String message, String title, int type) {
-		showOptionDialog(owner, message, title, 0, type, new String[] {"OK"}, 0);
+		showOptionDialog(owner, message, title, 0, type, new String[] { "OK" }, 0);
 	}
+
 	public static int showConfirmDialog(Component owner, Object message) {
 		return showConfirmDialog(owner, message, null);
 	}
+
 	public static int showConfirmDialog(Component owner, Object message, String title) {
-		return showOptionDialog(owner, message, title, 0, QUESTION_MESSAGE,
-				new String[] {"Yes", "No", "Cancel"}, 0);
+		return showOptionDialog(owner, message, title, 0, QUESTION_MESSAGE, new String[] { "Yes", "No", "Cancel" }, 0);
 	}
+
 	public static void showMessageDialog(Component owner, String message) {
 		showMessageDialog(owner, null, message, INFO_MESSAGE);
 	}
+
 	public static String showInputDialog(Component owner, String message) {
 		return showInputDialog(owner, message, null);
 	}
+
 	public static String showInputDialog(Component owner, String message, String title) {
 		JDialog d = createDialog(owner, title);
 		d.setLayout(null);
@@ -129,17 +133,17 @@ public class LDialogs {
 		d.add(label);
 		label.setBounds(0, 0, label.getPreferredSize().width, label.getPreferredSize().height);
 		theme.affect(label);
-		
+
 		LTextField textField = new LTextField("Enter your answer here...");
 		theme.affect(textField);
 		textField.setBounds(30, label.getPreferredSize().height, 280, 25);
-		
+
 		d.add(textField);
-		
+
 		JButton button = new JButton("OK");
 		theme.affect(button);
 		button.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				d.dispose();
@@ -147,22 +151,20 @@ public class LDialogs {
 		});
 		d.add(button);
 		button.setBorder(BorderFactory.createLineBorder(null));
-		button.setBounds(145, label.getPreferredSize().height + 40,
-				60, 30);
-		
+		button.setBounds(145, label.getPreferredSize().height + 40, 60, 30);
+
 		d.setSize(350, label.getPreferredSize().height + 130);
 		if (owner == null) {
-			d.setLocation(
-					Toolkit.getDefaultToolkit().getScreenSize().width/2 - d.getWidth()/2,
-					Toolkit.getDefaultToolkit().getScreenSize().height/2 - d.getHeight()/2);
+			d.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - d.getWidth() / 2,
+					Toolkit.getDefaultToolkit().getScreenSize().height / 2 - d.getHeight() / 2);
 		}
 		d.setResizable(false);
-		d.setVisible(true);	
+		d.setVisible(true);
 		return textField.getText();
 	}
-	public static int showOptionDialog(Component owner,
-	        Object message, String title, int optionType, int messageType,
-	        Object[] options, Object initialValue){
+
+	public static int showOptionDialog(Component owner, Object message, String title, int optionType, int messageType,
+			Object[] options, Object initialValue) {
 		JDialog d = createDialog(owner, title);
 		d.setLayout(null);
 		d.setBackground(theme.getBackgroundColor());
@@ -180,7 +182,7 @@ public class LDialogs {
 				JButton button = new JButton(options[i].toString());
 				theme.affect(button);
 				button.addActionListener(new ActionListener() {
-					
+
 					@Override
 					public void actionPerformed(ActionEvent e) {
 						d.dispose();
@@ -189,19 +191,18 @@ public class LDialogs {
 				});
 				d.add(button);
 				button.setBorder(BorderFactory.createLineBorder(null));
-				button.setBounds(60 + totalX, label.getPreferredSize().height,
-						button.getPreferredSize().width + 10, 30);
+				button.setBounds(60 + totalX, label.getPreferredSize().height, button.getPreferredSize().width + 10,
+						30);
 				totalX += button.getPreferredSize().width + 30;
 			}
 		}
-		d.setSize((totalX < 300? 300:totalX), label.getPreferredSize().height + 80);
+		d.setSize((totalX < 300 ? 300 : totalX), label.getPreferredSize().height + 80);
 		if (owner == null) {
-			d.setLocation(
-					Toolkit.getDefaultToolkit().getScreenSize().width/2 - d.getWidth()/2,
-					Toolkit.getDefaultToolkit().getScreenSize().height/2 - d.getHeight()/2);
+			d.setLocation(Toolkit.getDefaultToolkit().getScreenSize().width / 2 - d.getWidth() / 2,
+					Toolkit.getDefaultToolkit().getScreenSize().height / 2 - d.getHeight() / 2);
 		}
 		d.setResizable(false);
-		d.setVisible(true);	
+		d.setVisible(true);
 		return option.getValue();
 	}
 }
